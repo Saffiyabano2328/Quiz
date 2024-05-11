8a5ccb271c0196862c8fe1adadb03f8860dff0a7
Saffiyabano2328 <117672668+Saffiyabano2328@users.noreply.github.com>
2024-05-12 01:25:54 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,3 +1,82 @@
+question_data = [
+    {
+        "category" : "Science : Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False"
+        "Incorrect_answer":[
+            "True"
+        ]
+    },
+    {
+        "category" : "Science : Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True"
+        "Incorrect_answer":[
+            "False"
+        ]
+    },
+    {
+        "category" : "Science : Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "AMD created the first computer 64-bit processor.",
+        "correct_answer": "True"
+        "Incorrect_answer":[
+            "False"
+        ]
+    },
+    {
+        "category" : "Science : Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "HTML stand for Hypertext Markup Language",
+        "correct_answer": "True"
+        "Incorrect_answer":[
+            "False"
+        ]
+    },
+            
+]
+
+
+
+from question_model import question 
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+
+    new_question = question (question_text, question_answer)
+    
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+     quiz.next_questions()
+
+print("YOU HAVE COMPLETED THE QUIZ")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+
+class Question:
+
+    def __init(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
+
 class quizbrain:
 
     def __init__(self, q_list):
